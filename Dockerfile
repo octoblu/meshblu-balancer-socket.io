24e72ffdb9edf155f39f0a28f38ee5566f8e1767
Roy van de Water <roy.vwater@gmail.com>
2016-03-29 13:59:51 -0700

    Add some things that may work

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM haproxy:1.6
+
+RUN apt-get update && \
+    apt-get install -y socat && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+EXPOSE 1883
+EXPOSE 59890
+
+ADD stick-table.sh .
+ADD run.sh .
+ADD haproxy.cfg.sh .
+CMD ["./run.sh"]
---
+++ run.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+assert_servers(){
+  local servers="$1"
+
+  if [ -z "$servers" ]; then
+    echo "SERVERS not found, cowardly refusing to do anything"
+    exit 1
+  fi
+}
+
+run_haproxy() {
+  haproxy -f /usr/local/etc/haproxy/haproxy.cfg &
+  child=$!
+  echo "Waiting for child process: $child"
+  wait "$child"
+}
+
+write_haproxy() {
+  local servers="$1"
+  local password="$2"
+
+  env \
+    SERVERS="${servers}" \
+    PASSWORD="${password}" \
+    ./haproxy.cfg.sh > /usr/local/etc/haproxy/haproxy.cfg
+}
+
+main(){
+  local servers="${SERVERS}"
+  local password="${PASSWORD}"
+  assert_servers "${servers}"
+  assert_password "${password}"
+
+  write_haproxy "${servers}" "${password}"
+  run_haproxy
+}
+
+main $@
